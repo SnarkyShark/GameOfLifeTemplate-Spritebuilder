47d5084ca9db04dda201fc6e25cded90ea59c533
Will Johannesen <willjohannesen@gmail.com>
2015-08-08 14:54:48 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Grid.h
@@ -0,0 +1,13 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Will Johannesen on 8/8/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@end
---
+++ GameOfLife.spritebuilder/Grid.m
@@ -0,0 +1,13 @@
+//
+//  Grid.m
+//  GameOfLife
+//
+//  Created by Will Johannesen on 8/8/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Grid.h"
+
+@implementation Grid
+
+@end
